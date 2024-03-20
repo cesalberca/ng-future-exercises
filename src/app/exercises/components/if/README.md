0bbe75298e995a0bb01202ed73c88bf0edc2f03c
mariamoraan <mariamoranluaces@gmail.com>
2024-03-20 15:00:16 +0100

    feat(exercises): add statement for conditional rendering exercise

--- src/app/exercises/components/if/README.md
+++ src/app/exercises/components/if/README.md
@@ -1,11 +1,13 @@
-export const statement = `# Conditional rendering
+# Conditional rendering
 
 ## Introducción
+
 Vamos a mejorar la lista de hábitos. Si recibimos un array vacío, mostraremos un mensaje "No hay hábitos creados"
 
 ## Requisitos
+
 - Debe crearse un componente de página de Angular en el fichero if.component.ts de este directorio
 - El componente debe recibir por parámetro un array de hábitos
 - El componente debe tener una lista de hábitos
 - El componente debe usar un fichero .html para el template
-- El componente debe usar un fichero .css para los estilos`
+- El componente debe usar un fichero .css para los estilos
--- src/app/exercises/components/if/_solution/if.component.ts
+++ src/app/exercises/components/if/_solution/if.component.ts
@@ -1,13 +1,16 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core'
 import { HabitSolutionComponent } from '../../habit/_solution/habit.component'
+import { heroShieldExclamation } from '@ng-icons/heroicons/outline'
+import { provideIcons, NgIconComponent } from '@ng-icons/core'
 
 @Component({
   selector: 'app-if-solution',
   standalone: true,
   templateUrl: './if.component.html',
   styleUrl: './if.component.css',
-  imports: [HabitSolutionComponent],
+  imports: [HabitSolutionComponent, NgIconComponent],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  viewProviders: [provideIcons({ heroShieldExclamation })],
 })
 export class IfSolutionComponent {
   habits = input.required<string[]>()
