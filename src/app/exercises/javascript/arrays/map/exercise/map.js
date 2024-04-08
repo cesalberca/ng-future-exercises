8df459db5395b3b57a14b33bed28184dc23a804e
Antonio <Caparrini@users.noreply.github.com>
2024-04-08 10:32:25 +0000

    Done: Task map

--- src/app/exercises/javascript/arrays/map/exercise/map.js
+++ src/app/exercises/javascript/arrays/map/exercise/map.js
@@ -1,5 +1,7 @@
 function getPoweredByTwo(numbers) {
-  return numbers
+  return numbers.map(function (number) {
+    return Math.pow(number, 2)
+  })
 }
 
 console.log(getPoweredByTwo([1, 2, 3, 4, 5]))
