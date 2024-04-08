e473c267901c63bfe513a0c5b3de5d4ac59f7f55
Antonio <Caparrini@users.noreply.github.com>
2024-04-08 10:47:40 +0000

    Done Task filter

--- src/app/exercises/javascript/arrays/filter/exercise/filter.js
+++ src/app/exercises/javascript/arrays/filter/exercise/filter.js
@@ -1,5 +1,6 @@
-export function getEvenNumber() {
-  return
+export function getEvenNumber(numbers) {
+  const result = numbers.filter(number => number % 2 == 0)
+  return result
 }
 
 console.log(getEvenNumber([1, 2, 3, 4, 5, 6]))
