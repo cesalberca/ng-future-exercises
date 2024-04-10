20f9dca88aec70b551a4e5e10cd55c5585d392d8
conor <38634819+conoryo79@users.noreply.github.com>
2024-04-10 14:25:22 +0000

    a

--- src/app/exercises/typescript/generics/exercise/generics.ts
+++ src/app/exercises/typescript/generics/exercise/generics.ts
@@ -1,1 +1,36 @@
 // @ts-nocheck
+
+function identity1(arg: number): number {
+  return arg
+}
+
+function identity2(arg: string): string {
+  return arg
+}
+
+function identity3(arg: boolean): boolean {
+  return arg
+}
+
+function identity<Type>(arg: Type): Type {}
+
+class Generic {
+  foo?: number
+  bar(x: number): number {
+    return x
+  }
+}
+
+class Generic2 {
+  foo?: string
+  bar(x: string): string {
+    return x
+  }
+}
+
+class Generic<Type = string, ReturnType> {
+  foo?: Type
+  bar(x: Type): Type {
+    return x
+  }
+}
