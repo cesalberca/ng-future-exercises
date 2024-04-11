ac5f18af76f292f37046b6c381172b1e32323cf0
Joan Morató <moratojoan@gmail.com>
2024-04-11 10:56:55 +0200

    add content projection solution

--- src/app/exercises/components/content-projection/page/content-projection.page.ts
+++ src/app/exercises/components/content-projection/page/content-projection.page.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core'
 import { SolutionComponent } from '../../../../dashboard/delivery/components/solution/solution.component'
 
 import { MdComponent } from '../../../../dashboard/delivery/components/markdown/markdown.component'
+import { ContentProjectionSolutionComponent } from '../_solution/content-projection.component'
 
 @Component({
   selector: 'app-content-projection-exercise',
@@ -9,8 +10,9 @@ import { MdComponent } from '../../../../dashboard/delivery/components/markdown/
   template: `
     <app-solution-component>
       <app-markdown statement src="assets/app/exercises/components/content-projection/README.md" />
+      <app-content-projection-solution solution />
     </app-solution-component>
   `,
-  imports: [SolutionComponent, MdComponent],
+  imports: [SolutionComponent, MdComponent, ContentProjectionSolutionComponent],
 })
 export class ContentProjectionPage {}
