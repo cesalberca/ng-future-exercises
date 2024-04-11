27ccbd24ddcdd4bf29aa9379d3da00e0c0087ab4
Joan Morató <moratojoan@gmail.com>
2024-04-11 11:10:03 +0200

    add content-projection exercise

--- src/app/exercises/components/content-projection/page/content-projection.page.ts
+++ src/app/exercises/components/content-projection/page/content-projection.page.ts
@@ -3,6 +3,7 @@ import { SolutionComponent } from '../../../../dashboard/delivery/components/sol
 
 import { MdComponent } from '../../../../dashboard/delivery/components/markdown/markdown.component'
 import { ContentProjectionSolutionComponent } from '../_solution/content-projection.component'
+import { ContentProjectionComponent } from '../exercise/content-projection.component'
 
 @Component({
   selector: 'app-content-projection-exercise',
@@ -11,8 +12,9 @@ import { ContentProjectionSolutionComponent } from '../_solution/content-project
     <app-solution-component>
       <app-markdown statement src="assets/app/exercises/components/content-projection/README.md" />
       <app-content-projection-solution solution />
+      <app-content-projection user-solution />
     </app-solution-component>
   `,
-  imports: [SolutionComponent, MdComponent, ContentProjectionSolutionComponent],
+  imports: [SolutionComponent, MdComponent, ContentProjectionSolutionComponent, ContentProjectionComponent],
 })
 export class ContentProjectionPage {}
