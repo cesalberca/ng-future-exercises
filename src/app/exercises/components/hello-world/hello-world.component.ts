0f3fd8290dd593f8aa7cf09f2025c6e2d888a6d0
Antonio <Caparrini@users.noreply.github.com>
2024-04-13 16:11:10 +0000

    Done Task hello-world.component

--- src/app/exercises/components/hello-world/hello-world.component.ts
+++ src/app/exercises/components/hello-world/hello-world.component.ts
@@ -3,6 +3,13 @@ import { Component } from '@angular/core'
 @Component({
   selector: 'app-hello-world',
   standalone: true,
-  template: ``,
+  template: `<h1 class="hworld">Hello world!</h1>`,
+  styles: [
+    `
+      .hworld {
+        color: var(--primary-color);
+      }
+    `,
+  ],
 })
 export class HelloWorldComponent {}
