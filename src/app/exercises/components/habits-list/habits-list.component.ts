8a008e9e91242a8c1349d6b165bde95f5224f650
Antonio <Caparrini@users.noreply.github.com>
2024-04-13 16:55:19 +0000

    Done Task habits-list.component

--- src/app/exercises/components/habits-list/habits-list.component.ts
+++ src/app/exercises/components/habits-list/habits-list.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core'
+import { HabitComponent } from '../habit/habit.component'
+
+export interface CustomHabit {
+  name: string
+  healthy: boolean
+}
 
 @Component({
   selector: 'app-habits-list',
   standalone: true,
   templateUrl: './habits-list.component.html',
   styleUrl: './habits-list.component.css',
-  imports: [],
+  imports: [HabitComponent],
 })
-export class HabitsListComponent {}
+export class HabitsListComponent {
+  habit_list: CustomHabit[] = [
+    { name: 'Nadar', healthy: true },
+    { name: 'Comer', healthy: true },
+    { name: 'Coding', healthy: false },
+  ]
+}
