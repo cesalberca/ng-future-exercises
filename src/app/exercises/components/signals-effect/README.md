94529dbcbbe902c4ad82677c3ddc0a06c6c20e50
Joan Morató <moratojoan@gmail.com>
2024-04-15 10:53:58 +0200

    add effect exercise

---
+++ src/app/exercises/components/signals-effect/README.md
@@ -0,0 +1,7 @@
+# Computed Signals (read-only signals)
+
+## Introducción
+
+## Requisitos
+
+- Completa el componente de la carpeta `exercise` para utilizar un effect.
---
+++ src/app/exercises/components/signals-effect/exercise/signals-effect.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-signals-effect',
+  templateUrl: './signals-effect.component.html',
+  styleUrl: './signals-effect.component.css',
+  standalone: true,
+  imports: [],
+})
+export class SignalsEffectComponent {}
