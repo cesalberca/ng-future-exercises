cc596ced9e32811acef754f002ce10605422b00e
cesalberca <cesalberca@gmail.com>
2024-04-16 15:08:43 +0200

    Remove interceptors

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,14 +2,8 @@ import { ApplicationConfig } from '@angular/core'
 import { provideRouter } from '@angular/router'
 import { routes } from './app.routes'
 import { provideMarkdown } from 'ngx-markdown'
-import { HttpClient, provideHttpClient, withFetch, withInterceptors } from '@angular/common/http'
-import { baseUrlInterceptor } from './exercises/components/http-client/_solution/base-url.interceptor'
-import { errorInterceptor } from './exercises/components/http-client/_solution/error.interceptor'
+import { HttpClient, provideHttpClient, withFetch } from '@angular/common/http'
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideRouter(routes),
-    provideHttpClient(withFetch(), withInterceptors([baseUrlInterceptor, errorInterceptor])),
-    provideMarkdown({ loader: HttpClient }),
-  ],
+  providers: [provideRouter(routes), provideHttpClient(withFetch()), provideMarkdown({ loader: HttpClient })],
 }
