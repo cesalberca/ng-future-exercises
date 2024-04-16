d108de288281f753020f3789a7fe0e05ea92c623
cesalberca <cesalberca@gmail.com>
2024-04-16 12:15:20 +0200

    Add return type

--- src/app/exercises/components/pipe/_solution/is-today.pipe.ts
+++ src/app/exercises/components/pipe/_solution/is-today.pipe.ts
@@ -5,7 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core'
   standalone: true,
 })
 export class IsTodayPipe implements PipeTransform {
-  transform(value: Date): unknown {
+  transform(value: Date): string {
     if (value.toDateString() === new Date().toDateString()) {
       return `It's today!`
     }
