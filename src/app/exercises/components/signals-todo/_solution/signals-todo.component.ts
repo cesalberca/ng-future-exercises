410ee1b1a6ee2bda1133f0523a5f8acaf3c6672d
cesalberca <cesalberca@gmail.com>
2024-04-17 11:55:48 +0200

    Solve partially

--- src/app/exercises/components/signals-todo/_solution/signals-todo.component.ts
+++ src/app/exercises/components/signals-todo/_solution/signals-todo.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core'
+import { Component, computed, signal } from '@angular/core'
+
+interface Todo {
+  name: string
+  completed: boolean
+}
+
+type FilterStatus = 'all' | 'completed'
 
 @Component({
   selector: 'app-signals-todo-solution',
@@ -7,4 +14,41 @@ import { Component } from '@angular/core'
   standalone: true,
   imports: [],
 })
-export class SignalsTodoSolutionComponent {}
+export class SignalsTodoSolutionComponent {
+  todos = signal<Todo[]>([
+    {
+      name: 'bar',
+      completed: true,
+    },
+    {
+      name: 'baz',
+      completed: true,
+    },
+  ])
+  filterStatus = signal<FilterStatus>('all')
+  completedTodos = computed(() => this.todos().filter(x => x.completed).length)
+  filteredTodos = computed(() =>
+    this.todos().filter(x => {
+      if (this.filterStatus() === 'all') {
+        return true
+      } else if (this.filterStatus() === 'completed') {
+        return x.completed
+      } else {
+        return true
+      }
+    }),
+  )
+
+  create() {
+    const newTodo: Todo = {
+      name: 'Foo',
+      completed: false,
+    }
+
+    this.todos.update(x => [...x, newTodo])
+  }
+
+  changeStatus(status: FilterStatus) {
+    this.filterStatus.set(status)
+  }
+}
